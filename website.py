8f11232d6beedf7c79c865df2bcf2b6be0eff625
rayansudeora <59887243+rayansudeora@users.noreply.github.com>
2020-06-28 21:42:11 -0700

    Added logo and added designs

--- website.py
+++ website.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
-import requests
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
@@ -12,9 +11,11 @@ def index():
 def form():
 	return render_template("about.html")
 
-#@app.route("/courses.html")
-#def courses():
-	#return render_template("courses.html")
+'''
+@app.route("/courses.html")
+def courses():
+	return render_template("courses.html")
+'''
 
 @app.route("/course-details-python1.html")
 def course_details_python1():
